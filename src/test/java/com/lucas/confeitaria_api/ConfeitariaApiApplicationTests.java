72677a36b4a3a407f000fe7ab6737da17e1d03fb
Lucas Henrique <lucashsilvaa8@gmail.com>
2025-12-10 11:32:13 -0300

    primeiro commit

---
+++ src/test/java/com/lucas/confeitaria_api/ConfeitariaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucas.confeitaria_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfeitariaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
